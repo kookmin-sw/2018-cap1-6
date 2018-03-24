877192cf9bc2f0f606ac8a6c92e1a1378171542f
zesow <anstkd07@gmail.com>
2018-03-24 23:52:08 +0900

    view template 연습

--- src/webapp/gitwatcher/views.py
+++ src/webapp/gitwatcher/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+
+def index(request):
+    template = loader.get_template('gitwatcher/index.html')
+    context = {
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
