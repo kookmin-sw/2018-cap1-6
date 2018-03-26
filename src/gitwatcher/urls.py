c270e17b6049a48f4a23bbdcfaa48e4473c53ea2
one10004 <ryunhoo123@kookmin.ac.kr>
2018-03-26 14:26:16 +0900

    s3에서 zip 끌어오는 기능 추가

--- src/gitwatcher/urls.py
+++ src/gitwatcher/urls.py
@@ -4,5 +4,6 @@ from django.conf.urls import url
 #서버에 요청이 들어오면 어떻게 처리할지
 urlpatterns = [
 	url(r'^$', views.index),
-	url(r'^(?P<address>[\/\w_\.-]+)/submit$', views.gitwatch)
+	url(r'^(?P<address>[\/\w_\.-]+)/option1/submit$', views.gitwatch1),
+	url(r'^(?P<address>[\/\w_\.-]+)/option2/submit$', views.gitwatch2)
 ]
--- src/gitwatcher/views.py
+++ src/gitwatcher/views.py
@@ -7,12 +7,25 @@ def index(request):
 	return render(request,'gitwatcher/index.html')
 
 
-def gitwatch(request, address):
+
+def gitwatch1(request, address):
 	address = 'https://github.com/' + address
 	os.chdir('/home/ubuntu')
 	os.system('git clone ' + address + '.git')
 	words = address.split('/')
 	length = len(words)
 	os.chdir('/home/ubuntu/' + words[length-1])
-	os.system('python3 /home/ubuntu/gitinspector/gitinspector.py -F html > /home/ubuntu/2018-cap1-6/src/webapp/gitwatcher/templates/gitwatcher/statistics.html')
-	return render(request, 'gitwatcher/statistics.html')
+	os.system('python3 /home/ubuntu/gitinspector/gitinspector.py -F html > /home/ubuntu/2018-cap1-6/src/webapp/gitwatcher/templates/gitwatcher/statistics1.html')
+	return render(request, 'gitwatcher/statistics1.html')
+
+
+def gitwatch2(request, address):
+	words = address.split('/')
+	length = len(words)
+	address = words[length-1]
+	os.chdir('/home/ubuntu')
+	os.system('aws s3 cp s3://ec2s3example/' + address + '.zip ./')
+	os.system('unzip ' + address +'.zip -d ./' + address)
+	os.chdir('/home/ubuntu/' + address)
+	os.system('python3 /home/ubuntu/gitinspector/gitinspector.py -F html > /home/ubuntu/2018-cap1-6/src/webapp/gitwatcher/templates/gitwatcher/statistics2.html')
+	return render(request, 'gitwatcher/statistics2.html')
