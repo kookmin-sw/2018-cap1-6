de899260b9bdf00f1175f95b505f71a1ceba23fe
DESKTOP-G7MQ1D1\jhh51 <jh5154@kookmin.ac.kr>
2018-03-26 12:43:19 +0900

    Add new branch

--- src/webapp/gitwatcher/models.py
+++ src/webapp/gitwatcher/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 # Create your models here.
+
+class Githuburls(models.Model):
+    github_url = models.CharField(max_length = 200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.github_url
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
