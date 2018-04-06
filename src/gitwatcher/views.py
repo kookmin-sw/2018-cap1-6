b95adc1fd76ad6fe2c5988f8bb9ae553b06b3ba8
DESKTOP-G7MQ1D1\jhh51 <jh5154@kookmin.ac.kr>
2018-04-06 22:50:01 +0900

    boto 코드 추가

--- src/gitwatcher/views.py
+++ src/gitwatcher/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 import os
 import zipfile
 from gitinspector import gitinspector as git
+import boto3
+import botocore
 # Create your views here.
 
 def index(request):
@@ -25,10 +27,21 @@ def gitwatch2(request, address):
 	words = address.split('/')
 	length = len(words)
 	address = words[length-1]
+
+	BUCKET_NAME = 'gittos3fixed-outputbucket-ix0jgogj97ai'
+	KEY = 'kookmin-sw/2018-cap1-11/branch/master/kookmin-sw_2018-cap1-11_branch_master.zip'
+	s3 = boto3.resource('s3')
+	try:
+    	s3.Bucket(BUCKET_NAME).download_file(KEY, '\\home\\ubuntu\\다운로드성공.zip')
+	except botocore.exceptions.Clienterror as e:
+	    if e.response['Error']['Code'] == '404':
+	        print("The object does not exist.")
+	    else:
+	        raise
+
 	os.chdir('/home/ubuntu')
-	os.system('aws s3 cp s3://ec2s3example/' + address + '.zip ./')
-	os.system('unzip ' + address +'.zip -d ./' + address)
-	os.chdir('/home/ubuntu/' + address)
+	os.system('unzip 다운로드성공.zip -d ./다운로드성공')
+	os.chdir('/home/ubuntu/다운로드성공')
 	git.main()
 	#os.system('python3 /home/ubuntu/gitinspector/gitinspector.py -F html > /home/ubuntu/ttests/gitwatcher/templates/gitwatcher/statistics2.html')
 	return render(request, 'gitwatcher/statistics2.html')
