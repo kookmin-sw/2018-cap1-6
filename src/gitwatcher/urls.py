9094c74a0376994ec0150e8104dae1f35cccb058
one10004 <ryunhoo123@kookmin.ac.kr>
2018-04-10 22:23:33 +0900

    django 수정
    초기 화면에 clone 분석 버튼과 저장된 결과 확인 버튼 제공
    clone 분석은 기존처럼 git 주소 입력하면 clone해서 분석
    저장된 결과 확인은 ec2에 저장되어 있는 html 파일을 불러와 제공해준다

--- src/gitwatcher/urls.py
+++ src/gitwatcher/urls.py
@@ -4,6 +4,9 @@ from django.conf.urls import url
 #서버에 요청이 들어오면 어떻게 처리할지
 urlpatterns = [
 	url(r'^$', views.index),
-	url(r'^(?P<address>[\/\w_\.-]+)/option1/submit$', views.gitwatch1),
-	url(r'^(?P<address>[\/\w_\.-]+)/option2/submit$', views.gitwatch2)
+	url(r'^clonegit$',views.clonegit),
+	url(r'^clonegit/(?P<address>[\/\w_\.-]+)/result$', views.gitwatch1),
+	url(r'^storedData$',views.storedData),
+	url(r'^storedData/(?P<address>[\/\w_\.-]+)$',views.result)
+	#url(r'^(?P<address>[\/\w_\.-]+)/option2/submit$', views.gitwatch2)
 ]
--- src/gitwatcher/views.py
+++ src/gitwatcher/views.py
@@ -1,28 +1,81 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import os
-import zipfile
-from gitinspector import gitinspector as git
-import boto3
-import botocore
+from django.template import Context, loader
+#import boto3
+#import botocore
 # Create your views here.
 
 def index(request):
 	return render(request,'gitwatcher/index.html')
 
+def clonegit(request):
+	return render(request,'gitwatcher/clonegit.html')
 
 def gitwatch1(request, address):
 	address = 'https://github.com/' + address
-	os.chdir('/home/ubuntu')
+	os.chdir('/home/ec2-user')
 	os.system('git clone ' + address + '.git')
 	words = address.split('/')
 	length = len(words)
-	os.chdir('/home/ubuntu/' + words[length-1])
-	git.main()
-	#os.system('python3 /home/ubuntu/gitinspector/gitinspector.py -F html > /home/ubuntu/ttests/gitwatcher/templates/gitwatcher/statistics1.html')
-	return render(request, 'gitwatcher/statistics2.html')
+	os.chdir('/home/ec2-user/' + words[length-1])
+	os.system('python3 /home/ec2-user/gitinspector/gitinspector.py -F html > /home/ec2-user/ttests/gitwatcher/templates/gitwatcher/statistics1.html --grading')
+	os.chdir('/home/ec2-user')
+	os.system('rm -rf ' + words[length-1])
 
+	return render(request, 'gitwatcher/statistics1.html')
 
+def storedData(request):
+	path = '/home/ec2-user/files/html'
+	files = os.listdir(path)
+	k = lambda x: int(x[10] if x[11] is "_" else int(x[10:12]))
+	#files.sort(key=k)
+	context = {}
+	context['mydict'] = {}
+	os.chdir(path)
+
+	for i in range(0,len(files)):
+		os.system('cp ' + files[i] + ' /home/ec2-user/ttests/gitwatcher/templates/gitwatcher/')  #django의 template으로 복사하기
+		temp = files[i].split('_')
+		if temp[1] in context['mydict']:
+			if temp[3].find('.') != -1: #html이 붙어 있는 것을 떼어주기 위한 작업
+				remove_html = temp[3].split('.')
+				branch = remove_html[0]
+			else:
+				branch = temp[3]
+			for j in range(4,len(temp)):
+				if j == len(temp)-1:  # 마지막 branch명에 html이 붙어 있는 것을 떼어주기 위한 방법
+					remove_html = temp[j].split('.')
+					branch += '_'
+					branch += remove_html[0]
+				else:
+					branch += '_'
+					branch += temp[j]
+			context['mydict'][temp[1]].append(branch)
+		else:
+			if temp[3].find('.') != -1:
+				remove_html = temp[3].split('.')
+				branch = remove_html[0]
+			else:
+				branch = temp[3]
+			context['mydict'][temp[1]] = []
+			for j in range(4,len(temp)):
+				if j == len(temp)-1:
+					remove_html = temp[j].split('.')
+					branch += '_'
+					branch += remove_html[0]
+				else:
+					branch += '_'
+					branch += temp[j]
+			context['mydict'][temp[1]].append(branch)
+
+	return render(request,'gitwatcher/storedData.html',context)
+
+def result(request, address):
+	addr = 'gitwatcher/' + address + '.html'
+	return render(request, addr)
+
+'''
 def gitwatch2(request, address):
 	words = address.split('/')
 	length = len(words)
@@ -45,3 +98,4 @@ def gitwatch2(request, address):
 	git.main()
 	#os.system('python3 /home/ubuntu/gitinspector/gitinspector.py -F html > /home/ubuntu/ttests/gitwatcher/templates/gitwatcher/statistics2.html')
 	return render(request, 'gitwatcher/statistics2.html')
+'''
