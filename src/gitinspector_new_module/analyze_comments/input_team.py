b869af96d3f7b6d8452577d63319a50f66306d3f
zesow <anstkd07@gmail.com>
2018-05-20 21:54:27 +0900

    paging completed

--- src/gitinspector_new_module/analyze_comments/input_team.py
+++ src/gitinspector_new_module/analyze_comments/input_team.py
@@ -7,7 +7,7 @@ team = "2018-cap1-" + str(tnum)
 url = "https://api.github.com/repos/kookmin-sw/" + team + "/issues/comments?per_page=100"
 response = requests.get(url)
 comments = response.json()
-link = response.headers.get('link', None)
+link = response.headers.get('link',None)
 
 #총 issue 갯수
 totalnum = len(comments)
@@ -24,6 +24,12 @@ for i in range(len(comments)):
         a[name] = 1
 
 print("per user comment number : ", a)
-
+print(type(link))
 if link is not None:
     print(link)
+
+links = link.split(',')
+for link in links:
+    # If there is a 'next' link return the URL between the angle brackets, or None
+    if 'rel="next"' in link:
+        print(link[link.find("<")+1:link.find(">")])
