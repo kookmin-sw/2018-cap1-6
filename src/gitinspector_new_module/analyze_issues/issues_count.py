a4e0e810263de9800d3b1fc7e53b40d5de5cb5f8
zesow <anstkd07@gmail.com>
2018-05-20 13:03:00 +0900

    issue count

--- src/gitinspector_new_module/analyze_issues/issues_count.py
+++ src/gitinspector_new_module/analyze_issues/issues_count.py
@@ -6,30 +6,24 @@ import urllib2
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-for i in range(1,30):
-    team = "2018-cap1-" + str(i)
-    url = "https://api.github.com/repos/kookmin-sw/" + team + "/issues?state=all"
-    try:
-        issues = json.load(urllib2.urlopen(url))
-    except:
-        continue;
+tnum = input("insert team num : ")
+team = "2018-cap1-" + str(tnum)
+url = "https://api.github.com/repos/kookmin-sw/" + team + "/issues?state=all&per_page=100"
+issues = json.load(urllib2.urlopen(url))
 
-    if len(issues) == 0:
-        continue
-    print("team name is : ",team)
-    #총 issue 갯수
-    totalnum = issues[0]['number']
-    print("total issue number : ", totalnum)
+#총 issue 갯수
+totalnum = len(issues)
+print("total issue number : ", totalnum)
 
-    #유져 별 issue 갯수
-    a = {}
-    for i in range(len(issues)):
-        name = issues[i]['user']['login']
+#유져 별 issue 갯수
+a = {}
+for i in range(len(issues)):
+    name = issues[i]['user']['login']
 
-        if name in a:
-            a[name] = a[name] + 1
-        else:
-            a[name] = 1
+    if name in a:
+        a[name] = a[name] + 1
+    else:
+        a[name] = 1
 
-    print("per user issue number : ", a)
+print("per user issue number : ", a)
 
