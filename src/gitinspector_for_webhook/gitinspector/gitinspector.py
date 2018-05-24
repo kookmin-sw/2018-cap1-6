47df7181a74f618f8a6c4c662c6897dab638f33e
one10004 <ryunhoo123@kookmin.ac.kr>
2018-05-24 14:18:53 +0900

    make score js file

--- src/gitinspector_for_webhook/gitinspector/gitinspector.py
+++ src/gitinspector_for_webhook/gitinspector/gitinspector.py
@@ -28,6 +28,7 @@ from .changes import Changes
 from .low_changes import LowChanges # 추가
 from .config import GitConfig
 from .metrics import MetricsLogic
+from .score import Score # 추가
 from . import (basedir, clone, extensions, filtering, format, help, interval,
                localization, optval, terminal, version)
 from .output import outputable
@@ -71,6 +72,7 @@ class Runner(object):
         os.chdir(repos[0].location)  #추가
         low_change = LowChanges(repos)  #추가
         summed_low_change = low_change  #추가
+        score = Score(repos) # 추가
         os.chdir(previous_directory)  #추가
 
         for repo in repos:
